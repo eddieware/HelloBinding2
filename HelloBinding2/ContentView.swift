49768e98589138ba21155333adc0740ba83ad3fe
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-02 11:38:48 -0600

    update comment

--- HelloBinding2/ContentView.swift
+++ HelloBinding2/ContentView.swift
@@ -4,13 +4,26 @@
 //
 //  Created by EDUARDO MEJIA on 02/04/20.
 //  Copyright © 2020 EDDIEWARE. All rights reserved.
-//
+
+//TWO Way Binding
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var name: String = ""
+    private func printName(){
+        print(self.name)
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text(name)
+            TextField("Enter your Name", text: $name) //el place holder Enter name en text se guarda lo que escribimos $name y se lo pasa a name normal y se renderiza
+            .padding(12)
+            Button(action: printName){
+                Text("Show Name Value")
+            }
+        }
     }
 }
 
